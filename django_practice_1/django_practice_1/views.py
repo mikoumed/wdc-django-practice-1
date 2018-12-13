79718a091ca16fbb1fe186bc0ba82e9a98d7784d
mikoumed <mikoumed87@gmail.com>
2018-12-13 12:52:21 +0000

    get all views working

--- django_practice_1/django_practice_1/views.py
+++ django_practice_1/django_practice_1/views.py
@@ -1,13 +1,14 @@
 from datetime import datetime, timedelta
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseBadRequest
+from django.urls import reverse
 
 
 # Use /hello-world URL
 def hello_world(request):
     """Return a 'Hello World' string using HttpResponse"""
-    pass
+    return HttpResponse('Hello World')
 
 
 # Use /date URL
@@ -17,7 +18,7 @@ def current_date(request):
 
         i.e: 'Today is 5, January 2018'
     """
-    pass
+    return HttpResponse('Today is {}'.format(datetime.now().strftime('%d, %B %Y')))
 
 
 # Use URL with format /my-age/<year>/<month>/<day>
@@ -28,7 +29,8 @@ def my_age(request, year, month, day):
 
         i.e: /my-age/1992/1/20 returns 'Your age is 26 years old'
     """
-    pass
+    current_year = datetime.now().year
+    return HttpResponse('Your age is {} years old.'.format(current_year-year))
 
 
 # Use URL with format /next-birthday/<birthday>
@@ -38,7 +40,16 @@ def next_birthday(request, birthday):
         based on a given string GET parameter that comes in the URL, with the
         format 'YYYY-MM-DD'
     """
-    pass
+    try:
+        birthday = datetime.strptime(birthday, '%Y-%m-%d')
+    except ValueError:
+        return HttpResponseBadRequest()
+    today = datetime.now()
+    upcoming_birthday = birthday.replace(year=today.year)
+    if today > birthday:
+        upcoming_birthday = upcoming_birthday.replace(year=today.year + 1)
+    delta = upcoming_birthday - today
+    return HttpResponse('Days until next birthday: {}'.format(delta.days + 1))
 
 
 # Use /profile URL
@@ -47,7 +58,10 @@ def profile(request):
         This view should render the template 'profile.html'. Make sure you return
         the correct context to make it work.
     """
-    pass
+    return render(request, 'profile.html', {
+        'my_name' : 'mikou',
+        'my_age' : 31
+    })
 
 
 
@@ -83,8 +97,12 @@ AUTHORS_INFO = {
 
 # Use provided URLs, don't change them
 def authors(request):
-    pass
+    return render(request, 'authors.html', {
+        'authors': AUTHORS_INFO
+    })
 
 
 def author(request, authors_last_name):
-    pass
+    if authors_last_name in AUTHORS_INFO:
+        return render(request, 'author.html', AUTHORS_INFO[authors_last_name])
+    return redirect(reverse('authors'))
